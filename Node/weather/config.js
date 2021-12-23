98a6fb5dc5ffc5c3fcab89c65d59ad0ebe39bc6e
shriya1799 <shriya.p.addweb@gmail.com>
2021-12-23 18:51:16 +0530

    submitted

---
+++ Node/weather/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "0df5e3800678cba0ef86420d89f3d36b"
+    }
+}
+
+module.exports = constants;
